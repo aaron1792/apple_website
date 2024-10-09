1d680aad9cdedd55d8566c0502a4eacf476bd9e7
aaron1792 <aaron1792@gmail.com>
2024-10-09 16:24:33 +0100

    Apple_website

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { hightlightsSlides } from '../constants';
+
+function VideoCarousel() {
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, i) => (
+          <div
+            key={list.id}
+            id="slider">
+            <div className="video-carousel_container"></div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default VideoCarousel;
